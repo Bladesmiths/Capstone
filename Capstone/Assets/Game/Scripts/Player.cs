d66fc4171276e6418dae9103860400c04b68c252
Flamingfury56 <nwe4309@g.rit.edu>
2021-08-30 14:46:56 -0400

    Releasing right click calls the "Parry" debug statement.

--- Capstone/Assets/Game/Scripts/Player.cs
+++ Capstone/Assets/Game/Scripts/Player.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 namespace Bladesmiths.Capstone
 {
     public class Player : Character
     {
+        private WalkRunInputs walkRunInputs;
+        private InputAction parryAction;
+
         // Reference to the Finite State Machine
         private FiniteStateMachine FSM;
         //[SerializeField] private TransitionManager playerTransitionManager;
@@ -17,6 +21,9 @@ namespace Bladesmiths.Capstone
 
         private void Awake()
         {
+            // Create the WalkRunInputs InputAction object 
+            walkRunInputs = new WalkRunInputs();
+
             // Creates the FSM
             FSM = new FiniteStateMachine();
 
@@ -29,14 +36,33 @@ namespace Bladesmiths.Capstone
             FSM.AddTransition(move, idle, IsIdle());
             FSM.AddTransition(idle, move, IsMoving());
 
-            FSM.AddTransition(idle, parry, IsBlockReleased());
+            //FSM.AddTransition(idle, parry, IsBlockReleased());
 
             // Sets the current state
             FSM.SetCurrentState(idle);
 
-            
-          
+        }
 
+        private void OnEnable()
+        {
+            // Enable the controls needed for parrying
+            parryAction = walkRunInputs.Player.Parry;
+            parryAction.Enable();
+
+            walkRunInputs.Player.Parry.canceled += Parry;
+            walkRunInputs.Player.Parry.Enable();
+        }
+
+        private void OnDisable()
+        {
+            // Disable the controls needed for parrying
+            parryAction.Disable();
+            walkRunInputs.Player.Parry.Disable();
+        }
+
+        private void Parry(InputAction.CallbackContext obj)
+        {
+            Debug.Log("Parry");
         }
 
         /// <summary>
@@ -56,8 +82,7 @@ namespace Bladesmiths.Capstone
         /// Should be replaced with the input system call later on and not hard coded to right click
         /// </summary>
         /// <returns></returns>
-        public Func<bool> IsBlockReleased() => () => Input.GetMouseButtonUp(1) == true;
-
+        //public Func<bool> IsBlockReleased() => () => Input.GetMouseButtonUp(1) == true;
 
         private void Update()
         {
