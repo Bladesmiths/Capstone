b130803a48f92ccaf173c7ed566ae76a715be28f
John Vance <jmv7411@rit.edu>
2021-10-01 17:25:10 -0400

    Dodge Animation works

--- Capstone/Assets/Game/Scripts/Player.cs
+++ Capstone/Assets/Game/Scripts/Player.cs
@@ -272,7 +272,7 @@ namespace Bladesmiths.Capstone
         /// </summary>
         /// <returns></returns>
         // TODO: Should implement something like when dodging animation stops
-        public Func<bool> IsDodgingStopped() => () => dodge.timer >= 0.3;
+        public Func<bool> IsDodgingStopped() => () => dodge.timer >= 1.1f;
 
         /// <summary>
         /// The condition for having been attacked
@@ -414,7 +414,10 @@ namespace Bladesmiths.Capstone
             }
 
             // if the speed is less than the walkspeed and greater than 0 then set it to the walk speed
-            speed = Mathf.Clamp(speed, WalkSpeed, targetSpeed);
+            if (speed > 0)
+            {
+                speed = Mathf.Clamp(speed, WalkSpeed, targetSpeed);
+            }
 
             // If the player isn't moving set their speed to 0
             if (inputs.move == Vector2.zero) speed = 0.0f;
