0d85f2cdc1a2f0b0eddb8f30e7a91e3285807073
Trenton Plager <tplager1@gmail.com>
2021-10-06 18:50:18 -0400

    Implemented Block Animation
    Block animation is in now, functionality still isn't though

--- Capstone/Assets/Game/Scripts/StateMachine/PlayerStates/PlayerFSMState_BLOCK.cs
+++ Capstone/Assets/Game/Scripts/StateMachine/PlayerStates/PlayerFSMState_BLOCK.cs
@@ -12,9 +12,24 @@ namespace Bladesmiths.Capstone
     /// </summary>
     public class PlayerFSMState_BLOCK : PlayerFSMState
     {
+        private Player _player;
+        private PlayerInputsScript _input;
+        private Animator _animator;
+        private GameObject _sword;
+
+        private int _animIDBlock;
+        private bool _hasAnimator;
+
         private GameObject playerBlockBox;
-        public PlayerFSMState_BLOCK(GameObject playerBlockDetector)
+        public PlayerFSMState_BLOCK(Player player, PlayerInputsScript input, Animator animator, 
+            GameObject sword, GameObject playerBlockDetector)
         {
+            _player = player;
+            _input = input;
+            _animator = animator;
+            _sword = sword;
+            _sword.GetComponent<Rigidbody>().detectCollisions = false;
+
             playerBlockBox = playerBlockDetector;
             id = PlayerCondition.F_Blocking;
         }
@@ -27,16 +42,34 @@ namespace Bladesmiths.Capstone
         public override void OnEnter()
         {
             // Turns the block detector box on
-            playerBlockBox.SetActive(true);
+            //playerBlockBox.SetActive(true);
+
+            _animIDBlock = Animator.StringToHash("Block");
+
+            if (_animator != null)
+            {
+                _hasAnimator = true;
+            }
+            else
+            {
+                _hasAnimator = false;
+            }
+
+            _animator.SetBool(_animIDBlock, true);
+
+            _sword.GetComponent<Rigidbody>().detectCollisions = true;
         }
 
         public override void OnExit()
         {
             // Turns the block detector box off
-            playerBlockBox.SetActive(false);
+            //playerBlockBox.SetActive(false);
 
             // Change the color back to white
             playerBlockBox.GetComponent<MeshRenderer>().material.color = Color.white;
+
+            _sword.GetComponent<Rigidbody>().detectCollisions = false;
+            _animator.SetBool(_animIDBlock, false);
         }
 
     }
