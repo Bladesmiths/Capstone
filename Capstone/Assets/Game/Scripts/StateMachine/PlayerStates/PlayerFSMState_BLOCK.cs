b3e197da3db246691ca46a18d9030cc09572b38c
Trenton Plager <tplager1@gmail.com>
2021-10-07 19:42:43 -0400

    Adding documentation to blocking

--- Capstone/Assets/Game/Scripts/StateMachine/PlayerStates/PlayerFSMState_BLOCK.cs
+++ Capstone/Assets/Game/Scripts/StateMachine/PlayerStates/PlayerFSMState_BLOCK.cs
@@ -17,9 +17,11 @@ namespace Bladesmiths.Capstone
         private Animator _animator;
         private GameObject _sword;
 
+        // The ID of the block paramater in the Player's animator controller
         private int _animIDBlock;
         private bool _hasAnimator;
 
+        // The block rectangle that notifies if the player has blocked something
         private GameObject playerBlockBox;
         public PlayerFSMState_BLOCK(Player player, PlayerInputsScript input, Animator animator, 
             GameObject sword, GameObject playerBlockDetector)
@@ -36,7 +38,7 @@ namespace Bladesmiths.Capstone
 
         public override void Tick()
         {
-
+            // Not sure what to do here yet, if anything
         }
 
         public override void OnEnter()
@@ -44,8 +46,10 @@ namespace Bladesmiths.Capstone
             // Turns the block detector box on
             //playerBlockBox.SetActive(true);
 
+            // Assign block paramater id
             _animIDBlock = Animator.StringToHash("Block");
-
+            
+            // Do we need this?
             if (_animator != null)
             {
                 _hasAnimator = true;
@@ -55,8 +59,10 @@ namespace Bladesmiths.Capstone
                 _hasAnimator = false;
             }
 
+            // Set blocking id to true
             _animator.SetBool(_animIDBlock, true);
 
+            // Set the sword to detect collisions
             _sword.GetComponent<Rigidbody>().detectCollisions = true;
         }
 
@@ -68,6 +74,8 @@ namespace Bladesmiths.Capstone
             // Change the color back to white
             playerBlockBox.GetComponent<MeshRenderer>().material.color = Color.white;
 
+            // Set the sword to not detect collisions
+            // and turn off blocking paramater
             _sword.GetComponent<Rigidbody>().detectCollisions = false;
             _animator.SetBool(_animIDBlock, false);
         }
