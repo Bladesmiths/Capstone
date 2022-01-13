eda9a330ff9b5ff5554037651b6e8bd0da09c3e9
John Vance <jmv7411@rit.edu>
2022-01-13 13:47:10 -0500

    Rebased from dev and started working on cleaning things up

---
+++ Capstone/Assets/Scripts/StateMachine/PlayerStates 1/PlayerState_DEATH.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bladesmiths.Capstone.Enums;
+using Bladesmiths.Capstone.Testing;
+using StarterAssets;
+using System.Linq;
+using UnityEngine.Animations;
+
+namespace Bladesmiths.Capstone
+{
+    /// <summary>
+    /// The state for when the Player is dead
+    /// </summary>
+    public class PlayerState_DEATH : PlayerState_Base
+    {
+        Player _player;
+        private Animator _animator;
+        private int _animIDDamaged;
+        private int _animIDDeath;
+        private float _animDuration;
+        private float _timer;
+
+        //public PlayerState_DEATH(Player player, Animator animator)
+        //{
+        //    _player = player;
+        //    id = PlayerCondition.F_Dead;
+        //    _animator = animator;
+
+        //    // Assign damaged & dead paramater ids
+        //    _animIDDamaged = Animator.StringToHash("Damaged");
+        //    _animIDDeath = Animator.StringToHash("Dead");
+        //    _animDuration = animator.runtimeAnimatorController.animationClips.
+        //        Where(clip => clip.name == "Dying").ToArray()[0].length;
+        //}
+
+
+        private void OnEnable()
+        {
+            
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            // If the player is dead and just died (fadeToBlack is still occuring)
+            if (_timer >= _animDuration)
+            {
+                _player.FadeToBlack();
+            }
+            else
+            {
+                _timer += Time.deltaTime;
+            }
+
+            // Once the screen has faded to black
+            if (_player.hasFadedToBlack)
+            {
+                // Respawn the player to the most recent respawn point
+                _player.Respawn();
+            }
+            _player.damaged = false;
+
+        }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _player = _player = animator.GetComponent<Player>();
+            id = PlayerCondition.F_Dead;
+            _animator = animator;
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+
+            // Assign damaged & dead paramater ids
+            _animIDDamaged = Animator.StringToHash("Damaged");
+            _animIDDeath = Animator.StringToHash("Dead");
+            _animDuration = animator.runtimeAnimatorController.animationClips.
+                Where(clip => clip.name == "Dying").ToArray()[0].length;
+
+
+
+            _player.inState = true;
+            _player.justDied = true;
+            _player.ResetChipDamage();
+            _player.damaged = false;
+
+            _animator.SetBool(_animIDDeath, true);
+            _animator.SetTrigger(_animIDDamaged);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _player.inState = false;
+            _animator.SetBool(_animIDDeath, false);
+            _player.damaged = false;
+
+            _timer = 0;
+        }
+    }
+
+}
---
+++ Capstone/Assets/Scripts/StateMachine/PlayerStates 1/PlayerState_DODGE.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bladesmiths.Capstone.Enums;
+using Bladesmiths.Capstone.Testing;
+using StarterAssets;
+
+namespace Bladesmiths.Capstone
+{
+    /// <summary>
+    /// The state for when the player is dodging
+    /// TODO: will need to re-write as this is only a temp interaction for what it feels like
+    /// </summary>
+    public class PlayerState_DODGE : PlayerState_Base
+    {
+        public float timer;
+        public float dmgTimer;
+
+        private Player _player;
+        private PlayerInputsScript _input;
+        private Animator _animator;
+        private bool hasAnimator;
+
+        private int _animIDSpeed;
+        private int _animIDDodge;
+        private int _animIDMotionSpeed;
+        private int _animIDForward;
+        private int _animIDDodgeMove;
+        private float animBlend;
+        private bool _hasAnimator;
+        private CharacterController _controller;
+
+        private float _speed;
+        private float _targetRotation = 0.0f;
+        private float _rotationVelocity;
+        private float _verticalVelocity = 0.0f;
+        private float _terminalVelocity = 53.0f;
+        private float SpeedChangeRate = 10.0f;
+        private float RotationSmoothTime = 0.12f;
+
+        private bool Grounded = true;
+        private bool isGrounded = true;
+        public float GroundedOffset = -0.10f;
+        public float GroundedRadius = 0.20f;
+        public LayerMask GroundLayers;
+        private int _animIDGrounded;
+
+        public float JumpHeight = 1.2f;
+        public float Gravity = -15.0f;
+
+        private GameObject camera;
+
+        public bool canDmg = true;
+
+        private float currentHorizontalSpeed;
+        private Vector3 inputDirection;
+        private float rotationVelocity;
+
+
+        //public PlayerState_DODGE(Player player, PlayerInputsScript input, Animator animator, LayerMask layers)
+        //{
+        //    _player = player;
+        //    _input = input;
+        //    _animator = animator;
+        //    GroundLayers = layers;
+        //    id = PlayerCondition.F_Dodging;
+        //    canDmg = true;
+        //}
+
+        public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateMove(animator, stateInfo, layerIndex);
+
+
+            if (_input.move == Vector2.zero && _input.dodge)
+            {
+                _targetRotation = _player.transform.eulerAngles.y;
+
+                inputDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.back;
+
+
+            }
+            else if(_input.move != Vector2.zero && _input.dodge)
+            {
+                Vector3 inputMove = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+                // rotate to face input direction relative to camera position
+                _targetRotation = Mathf.Atan2(inputMove.x, inputMove.z) * Mathf.Rad2Deg + camera.transform.localEulerAngles.y;
+                //float rotation = Mathf.SmoothDampAngle(animator.transform.eulerAngles.y, _targetRotation, ref rotationVelocity, .01f);
+
+                _player.transform.rotation = Quaternion.Euler(0.0f, _targetRotation, 0.0f);
+                //animator.transform.eulerAngles = new Vector3(0, _targetRotation, 0.0f);
+                //_controller.transform.rotation = Quaternion.Euler(0.0f, _targetRotation, 0.0f);
+                Debug.Log(_targetRotation);
+
+
+
+                //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0.0f, targetRotation, 0.0f), 0.2f);
+
+                // rotate to face input direction relative to camera position
+
+                inputDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+            }
+
+        }
+
+        public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+        {
+            
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            timer += Time.deltaTime;
+            _input.dodge = false;
+            
+
+            dmgTimer += Time.deltaTime;
+            if (dmgTimer >= 0.3f)
+            {
+                _player.canDmg = true;
+            }           
+
+            if (_controller.isGrounded)
+            {
+                if (_verticalVelocity < 0.0f)
+                {
+                    _verticalVelocity = -2f;
+                }
+            }
+
+            Vector3 targetDirection = Vector3.zero;
+
+            float targetSpeed = 10;
+
+            // a reference to the players current horizontal velocity
+            currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+            float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+            if (_verticalVelocity < _terminalVelocity)
+            {
+                _verticalVelocity += Gravity * Time.deltaTime;
+            }
+
+            // move the player
+            _controller.Move(inputDirection.normalized * (targetSpeed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+        }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _player = animator.GetComponent<Player>();
+            _input = _player.GetComponent<PlayerInputsScript>();
+            _animator = animator;
+            id = PlayerCondition.F_Dodging;
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+
+            timer = 0;
+            dmgTimer = 0;
+            _player.canDmg = false;
+            _controller = _player.GetComponent<CharacterController>();
+            camera = GameObject.FindGameObjectWithTag("MainCamera");
+            hasAnimator = _player.TryGetComponent(out _animator);
+            _animIDDodge = Animator.StringToHash("Dodge");
+            _animIDForward = Animator.StringToHash("Forward");
+
+            // Testing
+            //((TestDataInt)GameObject.Find("TestingController").GetComponent<TestingController>().ReportedData["numDodges"]).Data.CurrentValue++;
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            canDmg = true;
+            _controller.SimpleMove(Vector3.zero);
+            _animator.SetBool(_animIDDodge, false);
+        }
+    }
+}
