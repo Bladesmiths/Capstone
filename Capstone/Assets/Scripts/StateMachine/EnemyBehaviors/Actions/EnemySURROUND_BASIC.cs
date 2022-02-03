997128af97952ee8b5925db36e7cdd7be8b01a3b
John Vance <jmv7411@rit.edu>
2022-02-03 14:05:10 -0500

    Enemy works similarly in behavior tree as it did with FSM
    Still have to get the SURROUND behavior working

--- Capstone/Assets/Scripts/StateMachine/EnemyBehaviors/Actions/EnemySURROUND_BASIC.cs
+++ Capstone/Assets/Scripts/StateMachine/EnemyBehaviors/Actions/EnemySURROUND_BASIC.cs
@@ -1,51 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
 using UnityEngine.AI;
 
 namespace Bladesmiths.Capstone
 {
-    public class EnemySURROUND_BASIC : EnemyState
+    public class EnemySURROUND_BASIC : Action
     {
-        private Player _player;
+        public SharedTransform player;
         private Enemy _enemy;
-        private float _speed;
-        private float surroundTimer;
-        private float surroundTimerMax;
+        private float sideMoveTimer;
+        private float sideMoveTimerMax;
+        private float seekAgainTimer;
+        private float seekAgainTimerMax;
+        private CharacterController controller;
+        private float speed;
+        private float dir;
+        private float surroundDistance;
 
         public EnemySURROUND_BASIC(Player player, Enemy enemy)
         {
-            _player = player;
             _enemy = enemy;
         }
-
-        public override void Tick()
+        public EnemySURROUND_BASIC()
         {
-            Vector3 dirToEnemy = _player.transform.position - _enemy.transform.position;
 
-            Debug.Log(Vector3.Distance(_enemy.transform.position, _player.transform.position + _player.transform.rotation * Vector3.forward));//_player.transform.forward * Vector3.Dot(dirToEnemy, _player.transform.forward)));
-            Debug.DrawLine(_enemy.transform.position, _player.transform.position + _player.transform.rotation * Vector3.forward);// _player.transform.forward * Vector3.Dot(dirToEnemy, _player.transform.forward));
+        }
 
-            //Debug.Log(_player.transform.forward * Vector3.Dot(dirToEnemy, _player.transform.forward));
+        public override TaskStatus OnUpdate()
+        {
+            MovementCheck();
+            return TaskStatus.Running;
+        }
 
-            if (Vector3.Distance(_enemy.transform.position, _player.transform.rotation * Vector3.forward) >= 1f)
+        private void AttackTimer()
+        {
+            _enemy.attackTimer -= Time.deltaTime;
+            if (_enemy.attackTimer <= 0)
             {
-                float dir = Vector3.Dot(dirToEnemy, _player.transform.right) < 0 ? -1 : 1;
-                _speed = 20 * Time.deltaTime * dir;
-
-                _enemy.transform.RotateAround(_player.transform.position, Vector3.up, _speed);
-
+                AIDirector.Instance.PopulateAttackQueue(_enemy);
             }
 
         }
 
-        public override void OnEnter()
+        public override void OnStart()
         {
-            _speed = 20f;
+            sideMoveTimer = 0;
+            sideMoveTimerMax = 1f;
+            seekAgainTimerMax = 1f;
+            seekAgainTimer = seekAgainTimerMax;
+            speed = 1.5f;
+            dir = Random.Range(-1, 2);
+            surroundDistance = 2.5f;
+            //player.Value = Player.instance.transform;
+            _enemy = gameObject.GetComponent<Enemy>();
+            controller = _enemy.GetComponent<CharacterController>();
+
         }
 
-        public override void OnExit()
+        public override void OnEnd()
         {
+            _enemy.moveVector = transform.position;
+            _enemy.rotateVector = player.Value.position - transform.position;
+        }
+
+        /// <summary>
+        /// Runs the movement code for seeking the player
+        /// </summary>
+        public void MovementCheck()
+        {
+            Vector3 movementVector = Vector3.zero;
+
+            Vector3 dist = player.Value.position - _enemy.transform.position;
+
+            
+            // Get the perpendicular vector to the distance between the Player and the Enemy
+            Vector3 move = Vector3.Cross(Vector3.up, dist) * dir;
+            movementVector = move + _enemy.transform.position;
+            AttackTimer();
+            seekAgainTimer -= Time.deltaTime;
+            
+            Debug.DrawLine(_enemy.transform.position, movementVector, Color.red);
+
+            _enemy.moveVector = movementVector;
+            _enemy.rotateVector = dist;
 
         }
 
---
+++ Capstone/Assets/Scripts/StateMachine/EnemyBehaviors/Conditionals/OutsideDistance.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+namespace Bladesmiths.Capstone
+{
+    public class OutsideDistance : Conditional
+    {
+        public SharedTransform player;
+        public float fieldOfView;
+        public float distance;
+
+
+        public override void OnAwake()
+        {
+            //player.Value = Player.instance.transform;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            Debug.Log(Vector3.Distance(player.Value.position, transform.position));
+            //if (Vector3.Distance(player.Value.position, transform.position) <= distance)
+            //{
+            //    return TaskStatus.Failure;
+            //}
+            if(InSight(player.Value, fieldOfView) && Vector3.Distance(player.Value.position, transform.position) <= distance)
+            {
+                return TaskStatus.Failure;
+            }            
+            return TaskStatus.Success;
+        }
+
+        public bool InSight(Transform target, float fov)
+        {
+            Vector3 dir = target.position - transform.position;
+            return Vector3.Angle(dir, transform.forward) < fov;
+        }
+
+
+    }
+}
