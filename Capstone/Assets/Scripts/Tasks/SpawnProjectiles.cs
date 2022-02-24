1d579d40aa1c2d01f57da29fe13d9d9f931a3044
Nick <nwe4309@rit.edu>
2022-02-24 13:39:20 -0500

    Created projectile attack for boss

--- Capstone/Assets/Scripts/Tasks/SpawnProjectiles.cs
+++ Capstone/Assets/Scripts/Tasks/SpawnProjectiles.cs
@@ -1,51 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 
 namespace Bladesmiths.Capstone.Testing
 {
     public class SpawnProjectiles : Action
     {
+
+        public SharedGameObject playerShared;
+        private GameObject player;
         [SerializeField] private Transform projectileSpawnPoint;
         [SerializeField] private GameObject projectile;
         [SerializeField] private float maxLifeTime;
 
         [SerializeField] private List<GameObject> activeProjectiles;
-        private float timer;
-
 
         public override void OnStart()
         {
-            timer = 0;
+            player = playerShared.Value;
             activeProjectiles = new List<GameObject>();
         }
 
         public override TaskStatus OnUpdate()
         {
-            if (timer == 0)
+            Vector3 spawnPos = projectileSpawnPoint.position;
+            Vector3 direction = (player.transform.position - spawnPos).normalized;
+            float angle = 0;
+
+            // Spawn i projectiles
+            for (int i = 0; i < 36; i ++)
             {
-                projectile.GetComponent<TestingProjectile>().Velocity = new Vector3(1000, 0, 0);
-                activeProjectiles.Add(GameObject.Instantiate(projectile, projectileSpawnPoint.position, Quaternion.Euler(0, 0, 0)));
-                activeProjectiles.Add(GameObject.Instantiate(projectile, projectileSpawnPoint.position, Quaternion.Euler(0, 0, 0)));
-                activeProjectiles.Add(GameObject.Instantiate(projectile, projectileSpawnPoint.position, Quaternion.Euler(0, 0, 0)));
+                // Have the projectiles face the player
+                GameObject tempProjectile = GameObject.Instantiate(projectile, spawnPos, Quaternion.Euler(0, 0, 0));
+
+                tempProjectile.GetComponent<TestingProjectile>().Velocity = direction * 20;
+                tempProjectile.GetComponent<TestingProjectile>().canMove = false;
+                tempProjectile.GetComponent<TestingProjectile>().timeTillDestruction = maxLifeTime;
+                activeProjectiles.Add(tempProjectile);
+
+                gameObject.GetComponent<Boss>().ObjectController.AddIdentifiedObject(Enums.Team.Enemy, tempProjectile.GetComponent<TestingProjectile>());
+
+                // Move next projectile up a bit and have it be facing 10 degrees to right of the last one
+                spawnPos.y += 0.3f;
+                direction = Quaternion.Euler(0, angle += 10 , 0) * direction;
             }
 
-            timer += Time.deltaTime;
+            gameObject.GetComponent<Boss>().activeProjectiles = activeProjectiles;
 
-            if (timer < maxLifeTime)
-            {
-                return TaskStatus.Running;
-            }
-            else
-            {
-                for (int i = 0; i < activeProjectiles.Count; i++)
-                {
-                    GameObject.Destroy(activeProjectiles[i]);
-                }
-                activeProjectiles.Clear();
-                return TaskStatus.Success;
-            }
+            return TaskStatus.Success;
         }
     }
 }
