306127296fa814fe6b2ba6a5364bda15035e8802
Nick <nwe4309@rit.edu>
2022-03-05 13:10:59 -0500

    Edited projectile behavior to shoot two rings if you're too close

--- Capstone/Assets/Scripts/Tasks/SpawnProjectiles.cs
+++ Capstone/Assets/Scripts/Tasks/SpawnProjectiles.cs
@@ -45,7 +45,15 @@ namespace Bladesmiths.Capstone.Testing
 
             Vector3 spawnPos = projectileSpawnPoint.position;
             Vector3 direction = (player.transform.position - spawnPos).normalized;
+            Vector3 stoppingVector = new Vector3(-0.9f, -0.3f, -0.2f);
             float angle = 0;
+
+            // Check if the player is within a certain distance
+            //Debug.Log(Vector3.Distance(gameObject.transform.position, player.transform.position));
+            if(Vector3.Distance(gameObject.transform.position,player.transform.position) <= 5)
+            {
+                direction = stoppingVector;
+            }
 
             // Spawn i projectiles
             for (int i = 0; i < 42; i ++)
@@ -65,6 +73,33 @@ namespace Bladesmiths.Capstone.Testing
                 spawnPos.z = projectileSpawnPoint.position.z + (0.4f * Mathf.Sin(angle));
                 spawnPos.x = projectileSpawnPoint.position.x + (0.4f * Mathf.Cos(angle));
                 direction = Quaternion.Euler(0, angle += 10 , 0) * direction;
+            }
+
+            // If the player is too close, spawns a small inner ring of projectiles
+            if (Vector3.Distance(gameObject.transform.position, player.transform.position) <= 5)
+            {
+                spawnPos = projectileSpawnPoint.position;
+                direction = (player.transform.position - spawnPos).normalized;
+                angle = 0;
+                // Spawn inner projectiles
+                for (int i = 0; i < 10; i++)
+                {
+                    // Have the projectiles face the player
+                    GameObject tempProjectile = GameObject.Instantiate(projectile, spawnPos, Quaternion.Euler(0, 0, 0));
+
+                    tempProjectile.GetComponent<TestingProjectile>().Velocity = direction * 20;
+                    tempProjectile.GetComponent<TestingProjectile>().canMove = false;
+                    tempProjectile.GetComponent<TestingProjectile>().timeTillDestruction = maxLifeTime;
+                    activeProjectiles.Add(tempProjectile);
+
+                    gameObject.GetComponent<Boss>().ObjectController.AddIdentifiedObject(Enums.Team.Enemy, tempProjectile.GetComponent<TestingProjectile>());
+
+                    // Move next projectile up a bit and have it be facing 10 degrees to right of the last one
+                    spawnPos.y += 0.1f;
+                    spawnPos.z = projectileSpawnPoint.position.z + (0.4f * Mathf.Sin(angle));
+                    spawnPos.x = projectileSpawnPoint.position.x + (0.4f * Mathf.Cos(angle));
+                    direction = Quaternion.Euler(0, angle += 10, 0) * direction;
+                }
             }
 
             gameObject.GetComponent<Boss>().activeProjectiles = activeProjectiles;
