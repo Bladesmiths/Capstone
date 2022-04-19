3159f3193a78c88bc2cfb54743b8c1c03395d385
John Vance <jmv7411@rit.edu>
2022-04-19 15:10:02 -0400

    Implemented a better shattering effect for the swords when switching

--- Capstone/Assets/Scripts/Player/PlayerShatterSword.cs
+++ Capstone/Assets/Scripts/Player/PlayerShatterSword.cs
@@ -7,15 +7,33 @@ namespace Bladesmiths.Capstone
     public class PlayerShatterSword : MonoBehaviour
     {
         public Vector3 center;
+        public Vector3 composite;
 
         private void Start()
         {
             center = transform.position;
             Rigidbody[] bodies = transform.GetComponentsInChildren<Rigidbody>();
+            composite = Vector3.zero;
+
+            foreach(Rigidbody rb in bodies)
+            {
+                composite += rb.gameObject.transform.position;
+            }
+
+            composite /= bodies.Length;
+
+            composite = new Vector3(composite.x - Player.instance.transform.forward.x, composite.y - 0.02f, composite.z - Player.instance.transform.forward.z);
+
+            //composite = Player.instance.transform.localRotation * composite;
+
 
             for (int i = 0; i < bodies.Length; i++)
             {
-                bodies[i].AddExplosionForce(5f, Player.instance.transform.position, 5f, 0.1f, ForceMode.Impulse);
+                Vector3 force = (bodies[i].gameObject.transform.position - composite).normalized;
+                force = new Vector3(force.x + Random.Range(-3f, 6f), Mathf.Abs(force.y) * 15f, force.z + Random.Range(-3f, 3f));
+                
+                bodies[i].AddForceAtPosition(force, composite, ForceMode.Impulse);
+                Debug.Log("Chunk Num: " + i + "    Force: " + force);
             }
         }
 
@@ -25,6 +43,12 @@ namespace Bladesmiths.Capstone
             {
                 Destroy(gameObject);
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(composite, 0.1f);
         }
     }
 }
