b26671054ec8e9b660bc683fb99fba2acb85f38a
John Vance <jmv7411@rit.edu>
2022-04-21 13:57:12 -0400

    Balanced SFX

--- Capstone/Assets/Scripts/UI/CustomMouseEnter.cs
+++ Capstone/Assets/Scripts/UI/CustomMouseEnter.cs
@@ -16,7 +16,7 @@ public class CustomMouseEnter : MonoBehaviour, IPointerEnterHandler
     // Start is called before the first frame update
     void Start()
     {
-        audioManager.PlaySound("");
+
     }
 
     // Update is called once per frame
