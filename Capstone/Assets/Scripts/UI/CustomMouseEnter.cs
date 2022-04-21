ce8b7ef43ccd962a22d49101cabbcfb50c825f66
John Vance <jmv7411@rit.edu>
2022-04-21 01:36:15 -0400

    Added 6 sfx to the project
    - Switch sword sfx
    - Boss Slam sfx
    - Boss sweep sfx
    - Hover UI buttons (In MainMenu)
    - UI Button Click (In MainMenu)
    - "Play" button clicking in main menu

--- Capstone/Assets/Scripts/UI/CustomMouseEnter.cs
+++ Capstone/Assets/Scripts/UI/CustomMouseEnter.cs
@@ -5,15 +5,18 @@ using UnityEngine.EventSystems;
 
 public class CustomMouseEnter : MonoBehaviour, IPointerEnterHandler
 {
+    public AudioManager audioManager;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         EventSystem.current.SetSelectedGameObject(gameObject);
+        audioManager.PlaySound("HoverButton");
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioManager.PlaySound("");
     }
 
     // Update is called once per frame
